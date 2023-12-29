8a50913d26c5e2de83842adca04add0032323c4d
Kanishk Singh <kanishk@basicx.in>
2023-12-29 15:44:09 +0530

    Added mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -9,6 +10,18 @@ const app = express();
 /* App config */
 app.use(express.json());
 app.use(cors());
+
+async function connectToMongoDB() {
+	mongoose
+		.connect(process.env.DB_URL)
+		.then(() => console.log("Connected to MongoDB successfully"))
+		.catch((err) => {
+			console.log("MongoDB connection ERROR");
+			console.error(err);
+		});
+}
+
+connectToMongoDB();
 
 /* Routes */
 app.post("/auth/register", function (req, res) {
