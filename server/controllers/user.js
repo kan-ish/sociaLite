ecb5f64f458dedfa004148eed699ca6312f14d8b
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-03 23:42:03 +0530

    Added getUser route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,13 @@
-export const getUser = (req, res) => {
-	res.send("received");
+import User from "../db/models/User.js";
+
+/* READ */
+export const getUser = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = await User.findById(id);
+
+		res.json({ user });
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
 };
