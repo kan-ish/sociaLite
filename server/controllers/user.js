653a8af43715fc5a064709e03f10b0ebbfe953f5
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-03 23:46:00 +0530

    Added route for adding/removing friends from a user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -32,3 +32,37 @@ export const getUserFriends = async (req, res) => {
 		res.status(404).json({ message: err.message });
 	}
 };
+
+/* UPDATE */
+export const addOrRemoveFriend = async (req, res) => {
+	try {
+		const { id, friendId } = req.params;
+		const user = await User.findById(id);
+		const friend = await User.findById(friendId);
+
+		if (user.friends.includes(friendId)) {
+			user.friends = user.friends.filter((id) => id !== friendId);
+			friend.friends = friend.friends.filter((uid) => uid !== id);
+		} else {
+			user.friends.push(friendId);
+			friend.friends.push(id);
+		}
+
+		await user.save();
+		await friend.save();
+
+		const friends = await Promise.all(
+			user.friends.map((id) => User.findById(id))
+		);
+
+		const condensedFriends = friends.map(
+			({ _id, firstName, lastName, occupation, location, picturePath }) => {
+				return { _id, firstName, lastName, occupation, location, picturePath };
+			}
+		);
+
+		res.json(condensedFriends);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,9 @@
 import express from "express";
-import { getUser, getUserFriends } from "../controllers/user.js";
+import {
+	getUser,
+	getUserFriends,
+	addOrRemoveFriend,
+} from "../controllers/user.js";
 import { authenticateUserToken } from "../middlewares/autenticateUserToken.js";
 
 const router = express.Router();
@@ -7,5 +11,8 @@ const router = express.Router();
 /* READ */
 router.get("/:id", authenticateUserToken, getUser);
 router.get("/:id/friends", authenticateUserToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", authenticateUserToken, addOrRemoveFriend);
 
 export default router;
