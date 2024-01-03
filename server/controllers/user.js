b055932286cfcadad669a9ce948a28d3b884bec4
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-03 23:44:18 +0530

    Added getUserFriends route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,3 +11,24 @@ export const getUser = async (req, res) => {
 		res.status(404).json({ message: err.message });
 	}
 };
+
+export const getUserFriends = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = await User.findById(id);
+
+		const friends = await Promise.all(
+			user.friends.map((id) => User.findById(id))
+		);
+
+		const condensedFriends = friends.map(
+			({ _id, firstName, lastName, occupation, location, picturePath }) => {
+				return { _id, firstName, lastName, occupation, location, picturePath };
+			}
+		);
+
+		res.json(condensedFriends);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUser } from "../controllers/user.js";
+import { getUser, getUserFriends } from "../controllers/user.js";
 import { authenticateUserToken } from "../middlewares/autenticateUserToken.js";
 
 const router = express.Router();
 
+/* READ */
 router.get("/:id", authenticateUserToken, getUser);
+router.get("/:id/friends", authenticateUserToken, getUserFriends);
 
 export default router;
