f7aaf3487a1e466574ea8e33de6fc4739eb2bb35
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-05 17:35:12 +0530

    Added controller for createPost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -2,7 +2,28 @@ import Post from "../db/models/Post.js";
 import User from "../db/models/User.js";
 
 export const createPost = async (req, res) => {
-	return;
+	try {
+		const { userId, description, picturePath } = req.body;
+		const user = await User.findById(userId);
+
+		const newPost = new Post({
+			userId,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			location: user.location,
+			description,
+			picturePath,
+			userPicturePath: user.picturePath,
+		});
+
+		await newPost.save();
+
+		const allPosts = await Post.find();
+
+		res.status(201).json({ allPosts });
+	} catch (err) {
+		res.status(409).json({ message: err.message });
+	}
 };
 
 export const getFeedPosts = (req, res) => {
