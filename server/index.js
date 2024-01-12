e4189923b1fd4d3e51658386673d90cc00768649
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-12 17:51:08 +0530

    changed mongoose connect function to async await

--- server/index.js
+++ server/index.js
@@ -16,13 +16,13 @@ app.use(express.json());
 app.use(cors());
 
 async function connectToMongoDB() {
-	mongoose
-		.connect(process.env.DB_URL)
-		.then(() => console.log("Connected to MongoDB successfully"))
-		.catch((err) => {
-			console.log("MongoDB connection ERROR");
-			console.error(err);
-		});
+	try {
+		await mongoose.connect(process.env.DB_URL);
+		console.log("Connected to MongoDB successfully");
+	} catch (err) {
+		console.log("MongoDB connection ERROR");
+		console.error(err);
+	}
 }
 connectToMongoDB();
 
