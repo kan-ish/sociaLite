ae65bd7d98c0e16fe40db42edaa23a453feaa3c5
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-18 23:22:58 +0530

    Feat: added basic views components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./views/homePage/HomePage";
+import LoginPage from "./views/loginPage/LoginPage";
+import ProfilePage from "./views/profilePage/ProfilePage";
 
 function App() {
 	return (
-		<>
-			<h1>Hello World</h1>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<LoginPage />} />
+				<Route path="/home" element={<HomePage />} />
+				<Route path="/profile/:userid" element={<ProfilePage />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
