ed400e55c7b1c757f49233d1888125990498f7f1
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-20 15:42:06 +0530

    Feat: add basic markup for login page

--- client/src/views/loginPage/LoginPage.jsx
+++ client/src/views/loginPage/LoginPage.jsx
@@ -1,8 +1,37 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+
 const LoginPage = () => {
+	const theme = useTheme();
+	const isMobileDevice = useMediaQuery("(max-width: 1000px)");
+
 	return (
-		<>
-			<div>Login Page</div>
-		</>
+		<Box>
+			<Box
+				width="100%"
+				backgroundColor={theme.palette.background.alt}
+				p="1rem 6%"
+				textAlign="center"
+			>
+				<Typography fontWeight="bold" fontSize="32px" color="primary">
+					SociaLite
+				</Typography>
+			</Box>
+
+			<Box
+				width={isMobileDevice ? "93%" : "50%"}
+				p="2rem"
+				m="2rem auto"
+				borderRadius="1.5rem"
+				backgroundColor={theme.palette.background.alt}
+			>
+				<Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+					Socialite{" "}
+					<small>
+						As if there weren&apos;t enough social media platforms already
+					</small>
+				</Typography>
+			</Box>
+		</Box>
 	);
 };
 
