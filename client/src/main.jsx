737202c9be914885b2a2c48d786f95d14f75dee2
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-22 17:15:00 +0530

    Feat: add fonts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import store from "./redux/index.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 
+import "./index.css"
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
