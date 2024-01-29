6fef598e0cfc02bd9d1597c193188953d9b40389
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-29 17:45:58 +0530

    Feat: add single post widget

---
+++ client/src/components/widgets/PostWidget.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import FlexboxSpaceBetween from "../styledWrappers/FlexboxSpaceBetween";
+import Friend from "../Friend";
+import WidgetWrapper from "../styledWrappers/WidgetWrapper";
+
+import {
+	ChatBubbleOutlineOutlined,
+	FavoriteBorderOutlined,
+	FavoriteOutlined,
+	ShareOutlined,
+} from "@mui/icons-material";
+import { IconButton, Typography, useTheme } from "@mui/material";
+import { setPost } from "@/redux/slices/authSlice";
+
+const PostWidget = ({
+	postId,
+	postUserId,
+	firstName,
+	lastName,
+	description,
+	location,
+	picturePath,
+	userPicturePath,
+	likes,
+	comments,
+}) => {
+	const dispatch = useDispatch();
+
+	const [isCommentsOpen, setIsCommentsOpen] = useState(false);
+
+	const token = useSelector((state) => state.token);
+	const loggedInUserId = useSelector((state) => state.user._id);
+	const { palette } = useTheme();
+
+	const isLiked = Boolean(likes[loggedInUserId]);
+	const fullName = `${firstName[0].toUpperCase()}${firstName.slice(
+		1
+	)} ${lastName[0].toUpperCase()}${lastName.slice(1)} `;
+
+	const patchLike = async () => {
+		const res = await fetch(`http://localhost:6001/posts/${postId}/like`, {
+			method: "PATCH",
+			headers: {
+				Authorization: `Bearer ${token}`,
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ userId: loggedInUserId }),
+		});
+		const updatedPost = await res.json();
+
+		dispatch(setPost({ post: updatedPost }));
+	};
+
+	return (
+		<>
+			<WidgetWrapper margin={"2rem 0"}>
+				<Friend
+					friendId={postUserId}
+					name={fullName}
+					subtitle={subtitle}
+					userPicturePath={userPicturePath}
+				/>
+
+				<Typography color={palette.neutral.main} sx={{ mt: "1rem" }}>
+					{description}
+				</Typography>
+
+				{picturePath && (
+					<img
+						src={`http://localhost:6001/assets/${picturePath}`}
+						alt={picturePath}
+						width={"100%"}
+						height={"auto"}
+						style={{
+							borderRadius: "0.75rem",
+							marginTop: "0.75rem",
+						}}
+					/>
+				)}
+
+				<FlexboxSpaceBetween mt={"0.25rem"}>
+					<FlexboxSpaceBetween gap={"1rem"}>
+						<FlexboxSpaceBetween gap={"0.3rem"}>
+							<IconButton onClick={patchLike}>
+								{isLiked ? (
+									<FavoriteOutlined
+										sx={{ color: palette.primary.main }}
+									/>
+								) : (
+									<FavoriteBorderOutlined />
+								)}
+							</IconButton>
+
+							<Typography>{Object.keys(likes).length}</Typography>
+						</FlexboxSpaceBetween>
+
+						<FlexboxSpaceBetween gap={"0.3rem"}>
+							<IconButton onClick={setIsCommentsOpen(!isCommentsOpen)}>
+								<ChatBubbleOutlineOutlined />
+							</IconButton>
+
+							<Typography>{comments.length}</Typography>
+						</FlexboxSpaceBetween>
+					</FlexboxSpaceBetween>
+
+                    <IconButton>
+                        <ShareOutlined />
+                    </IconButton>
+				</FlexboxSpaceBetween>
+			</WidgetWrapper>
+		</>
+	);
+};
+
+export default PostWidget;
