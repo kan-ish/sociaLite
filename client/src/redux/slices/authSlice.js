0655b3558d9a3f0e930272b05f49634874f988c0
Kanishk Singh <kanishk.singh198@gmail.com>
2024-01-29 13:47:48 +0530

    Feat: add new reducer to store for user friend list

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -1,24 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null,
-    token: null
-}
+	user: null,
+	token: null,
+};
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    reducers: {
-        setUserLogin: (state, action) => {
-            state.user = action.payload.user
-            state.token = action.payload.token
-        },
-        setUserLogout: (state) => {
-            state.user = null
-            state.token = null
-        }
-    }
-})
+	name: "auth",
+	initialState,
+	reducers: {
+		setUserLogin: (state, action) => {
+			state.user = action.payload.user;
+			state.token = action.payload.token;
+		},
+		setUserLogout: (state) => {
+			state.user = null;
+			state.token = null;
+		},
+		setFriends: (state, action) => {
+			state.user && (state.user.friends = action.payload.friends);
+		},
+	},
+});
 
-export const { setUserLogin, setUserLogout } = authSlice.actions
-export default authSlice.reducer
+export const { setUserLogin, setUserLogout, setFriends } = authSlice.actions;
+export default authSlice.reducer;
